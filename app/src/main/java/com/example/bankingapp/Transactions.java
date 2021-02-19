c161a6952d21192980b75ed2d1a9df809c3feae8
Pravi-n <118cs0029@iiitk.ac.in>
2021-02-19 12:37:43 +0530

    temp

---
+++ app/src/main/java/com/example/bankingapp/Transactions.java
@@ -0,0 +1,26 @@
+package com.example.bankingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class Transactions extends AppCompatActivity {
+
+    Transaction_Database tdb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Transactions");
+        setContentView(R.layout.activity_transactions);
+        ListView listView = findViewById(R.id.tlist);
+        String[] Users = new String[]{"from_user","to_user","amount","status"};
+        int[] to = new int[]{R.id.fromView,R.id.toVIew,R.id.amountView,R.id.statusView};
+        tdb = new Transaction_Database(this);
+        Cursor c = tdb.getinfo();
+        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this,R.layout.single_transaction,c,Users,to,0);
+        listView.setAdapter(simpleCursorAdapter);
+    }
+}
